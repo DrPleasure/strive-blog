94ed5f79c98787938703dab0a18f2c95737769ad
DrPleasure <114397562+DrPleasure@users.noreply.github.com>
2022-12-21 16:47:11 +0100

    updates

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import { useState, useEffect} from "react";
+
+
 
 const BlogList = (props) => {
+
+const [posts, setPosts] = useState([])
+
+  const fetchedData = async () => {
+
+    try {
+      const response = await fetch('http://localhost:3001/blogposts/');
+      const data = await response.json();
+      setPosts(data)
+      console.log()
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+  fetchedData()
+},[])
+
   return (
     <Row>
       {posts.map((post) => (
